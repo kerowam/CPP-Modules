fd503ecc5bf14ce3597f393395a32d62e8a739be
gfredes- <gfredes-@student.42malaga.com>
2024-05-14 16:07:03 +0200

    ex05 of CPP_01 finished

--- CPP_01/ex05/Harl.cpp
+++ CPP_01/ex05/Harl.cpp
@@ -6,16 +6,53 @@
 /*   By: gfredes- <gfredes-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 03:08:07 by gfredes-          #+#    #+#             */
-/*   Updated: 2024/05/14 03:10:50 by gfredes-         ###   ########.fr       */
+/*   Updated: 2024/05/14 15:55:00 by gfredes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-Harl::Harl(/* args */)
+Harl::Harl()
 {
+	lvls[0].lvl = "DEBUG";
+	lvls[0].ft = &Harl::debug;
+	lvls[1].lvl = "INFO";
+	lvls[1].ft = &Harl::info;
+	lvls[2].lvl = "WARNING";
+	lvls[2].ft = &Harl::warning;
+	lvls[3].lvl = "ERROR";
+	lvls[3].ft = &Harl::error;
 }
 
 Harl::~Harl()
 {
+}
+
+void	Harl::debug(void)
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger."
+	<< " I really do!" << std::endl;
+}
+void	Harl::info(void)
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put"
+	<< "enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+void	Harl::warning(void)
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for"
+	<< "years whereas you started working here since last month." << std::endl;
+}
+void	Harl::error(void)
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void	Harl::complain(std::string level)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		if (level == lvls[i].lvl)
+			(this->*lvls[i].ft)();
+	}
 }
--- CPP_01/ex05/Harl.hpp
+++ CPP_01/ex05/Harl.hpp
@@ -6,20 +6,33 @@
 /*   By: gfredes- <gfredes-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 03:07:54 by gfredes-          #+#    #+#             */
-/*   Updated: 2024/05/14 03:10:54 by gfredes-         ###   ########.fr       */
+/*   Updated: 2024/05/14 16:02:53 by gfredes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HARL_HPP
 # define HARL_HPP
+# include <iostream>
+# include <string>
 
 class Harl
 {
-private:
-	/* data */
-public:
-	Harl(/* args */);
-	~Harl();
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+		struct	_levelFt
+		{
+			std::string	lvl;
+			void (Harl::* ft)(void);
+		};
+		struct _levelFt	lvls[4];
+		
+	public:
+		Harl();
+		~Harl();
+		void	complain(std::string level);
 };
 
 #endif
