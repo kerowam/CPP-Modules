d12a2c860faa7e959865083515d1c155f2874c13
gfredes- <gfredes-@student.42malaga.com>
2024-06-01 03:48:58 +0200

    ex00 of CPP_03 finished

--- CPP_03/ex00/ClapTrap.hpp
+++ CPP_03/ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: gfredes- <gfredes-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/01 02:12:24 by gfredes-          #+#    #+#             */
-/*   Updated: 2024/06/01 02:36:23 by gfredes-         ###   ########.fr       */
+/*   Updated: 2024/06/01 03:36:51 by gfredes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ class ClapTrap
 		std::string	_name;
 		unsigned int	_hit_points;
 		unsigned int	_energy_points;
-		unsigned int	_atack_damage;
+		unsigned int	_attack_damage;
 
 	public:
 		ClapTrap();
@@ -29,9 +29,9 @@ class ClapTrap
 		~ClapTrap();
 
 		ClapTrap&	operator=(const ClapTrap& CT_name);
-		void	atack(const std::string& target);
+		void	attack(const std::string& target);
 		void	takeDamage(unsigned int amount);
-		void	beRepaired(unsigned int among);
-}
+		void	beRepaired(unsigned int amount);
+};
 
 #endif
--- CPP_03/ex00/main.cpp
+++ CPP_03/ex00/main.cpp
@@ -6,13 +6,33 @@
 /*   By: gfredes- <gfredes-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/01 02:12:13 by gfredes-          #+#    #+#             */
-/*   Updated: 2024/06/01 02:40:54 by gfredes-         ###   ########.fr       */
+/*   Updated: 2024/06/01 03:46:27 by gfredes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ClapTrap.cpp"
+#include "ClapTrap.hpp"
 
 int	main(void)
 {
-	
+	ClapTrap	cp_tp1("Clap");
+	ClapTrap	cp_tp2("Trap");
+
+	cp_tp1.attack("Trap");
+	cp_tp2.takeDamage(5);
+	cp_tp2.beRepaired(3);
+	cp_tp1.attack("Trap");
+	cp_tp2.takeDamage(5);
+	cp_tp1.attack("Trap");
+	cp_tp2.takeDamage(5);
+	cp_tp1.attack("Trap");
+	cp_tp1.beRepaired(2);
+	cp_tp2.beRepaired(3);
+	cp_tp2.attack("Clap");
+	cp_tp1.attack("Trap");
+	cp_tp1.attack("Trap");
+	cp_tp1.attack("Trap");
+	cp_tp1.attack("Trap");
+	cp_tp1.attack("Trap");
+	cp_tp1.attack("Trap");
+	cp_tp1.beRepaired(2);
 }
