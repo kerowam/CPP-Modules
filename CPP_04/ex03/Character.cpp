8530ef160da61483206581e5e885d0d1d95c3b63
gfredes- <gfredes-@student.42malaga.com>
2024-06-13 15:53:18 +0200

    Character class implemented

--- CPP_04/ex03/Character.cpp
+++ CPP_04/ex03/Character.cpp
@@ -6,7 +6,7 @@
 /*   By: gfredes- <gfredes-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 21:02:26 by gfredes-          #+#    #+#             */
-/*   Updated: 2024/06/12 21:35:21 by gfredes-         ###   ########.fr       */
+/*   Updated: 2024/06/13 15:52:13 by gfredes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,77 @@
 
 Character::Character()
 {
-
+	for (int i = 0; i < 4; i++)
+		this->_inventory[i] = 0;
 }
 
 Character::Character(Character& instance)
 {
-
+	*this = instance;
 }
 
 Character::~Character()
 {
-
+	for (int i = 0; i < 4; i++)
+	{
+		if (this->_inventory[i])
+			delete(_inventory[i]);
+	}
 }
 	
 Character&	Character::operator=(Character& instance)
 {
+	if (this != &instance)
+	{
+		this->_name = instance._name;
+		for (int i = 0; i < 4; i++)
+		{
+			if(this->_inventory[i])
+				delete(_inventory[i]);
+			this->_inventory[i] = instance._inventory[i]->clone();
+		}
+	}
+	return *this;
+}
 
+Character::Character(std::string name):
+_name(name)
+{
+	for (int i = 0; i < 4; i++)
+		this->_inventory[i] = 0;
+}
+
+std::string const & Character::getName() const
+{
+	return this->_name;
+}
+
+void Character::equip(AMateria* m)
+{
+	int	i = 0;
+	while (i < 4 && this->_inventory[i])
+		i++;
+	if (i < 4)
+		this->_inventory[i] = m;
+	else
+		std::cout << "No space on inventory! Nothing happens!" << std::endl;
+	return;
+}
+
+void Character::unequip(int idx)
+{
+	if (idx >= 0 && idx < 4 && this->_inventory[idx])
+		this->_inventory[idx] = 0;
+	else
+		std::cout << "There's no materia to unequip! Nothing happens!" << std::endl;
+	return;
+}
+
+void Character::use(int idx, ICharacter& target)
+{
+	if (idx >= 0 && idx < 4 && this->_inventory[idx])
+		this->_inventory[idx]->use(target);
+	else
+		std::cout << "There's no materia to use! Nothing happens!" << std::endl;
+	return;
 }
--- CPP_04/ex03/Character.hpp
+++ CPP_04/ex03/Character.hpp
@@ -6,7 +6,7 @@
 /*   By: gfredes- <gfredes-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 21:02:17 by gfredes-          #+#    #+#             */
-/*   Updated: 2024/06/12 21:33:20 by gfredes-         ###   ########.fr       */
+/*   Updated: 2024/06/13 14:46:46 by gfredes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,21 @@
 class Character: public ICharacter
 {
 	private:
+		std::string	_name;
+		AMateria	*_inventory[4];
 		
 	public:
 		Character();
 		Character(Character& instance);
+		Character(std::string name);
 		~Character();
 	
 		Character&	operator=(Character& instance);
+
+		std::string const & getName() const;
+		void equip(AMateria* m);
+		void unequip(int idx);
+		void use(int idx, ICharacter& target);
 };
 
 #endif
