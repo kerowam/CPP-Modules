ccbc8c24e9f4982203fcd20c78f5491b87a17d53
gfredes- <gfredes-@student.42malaga.com>
2024-08-05 20:13:43 +0200

    files and structure of ex02 in CPP_06 added

---
+++ CPP_06/ex02/main.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gfredes- <gfredes-@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/03 20:10:40 by gfredes-          #+#    #+#             */
+/*   Updated: 2024/08/05 20:11:04 by gfredes-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+Base	*generate(void)
+{
+
+}
+
+void	identify(Base* p)
+{
+
+}
+
+void	identify(Base& p)
+{
+
+}
+
+int	main(void)
+{
+	
+	return 0;
+}
