7711d444aba0c762a25061aff5901c641399766b
gfredes- <gfredes-@student.42malaga.com>
2024-08-05 19:59:28 +0200

    ex01 of CPP_06 done. Pending to check

--- CPP_06/ex01/Serializer.cpp
+++ CPP_06/ex01/Serializer.cpp
@@ -6,38 +6,33 @@
 /*   By: gfredes- <gfredes-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 19:25:55 by gfredes-          #+#    #+#             */
-/*   Updated: 2024/08/05 19:35:51 by gfredes-         ###   ########.fr       */
+/*   Updated: 2024/08/05 19:45:35 by gfredes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Serializer.hpp"
 
-Serializer::Serializer()
-{
-
-}
+Serializer::Serializer() {}
 
 Serializer::Serializer(const Serializer& serializer)
 {
-
+	(void)serializer;
 }
 
 Serializer&	Serializer::operator=(const Serializer& serializer)
 {
-
+	(void)serializer;
+	return *this;
 }
 
-Serializer::~Serializer()
-{
-
-}
+Serializer::~Serializer() {}
 
 uintptr_t	Serializer::serialize(Data* ptr)
 {
-
+	return reinterpret_cast<uintptr_t>(ptr);
 }
 
 Data*		Serializer::deserialize(uintptr_t raw)
 {
-
+	return reinterpret_cast<Data*>(raw);
 }
--- CPP_06/ex01/main.cpp
+++ CPP_06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gfredes- <gfredes-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/03 20:10:40 by gfredes-          #+#    #+#             */
-/*   Updated: 2024/08/05 19:25:06 by gfredes-         ###   ########.fr       */
+/*   Updated: 2024/08/05 19:57:11 by gfredes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,17 @@
 
 int	main(int argc, char** argv)
 {
+	Data	*data_ptr = new Data;
+	uintptr_t	raw = Serializer::serialize(data_ptr);
+	Data	*new_ptr = Serializer::deserialize(raw);
+
+	data_ptr->data = "Some data";
+	
+	std::cout << "*data_ptr:\t" << data_ptr << std::endl;
+	std::cout << "data_ptr->data:\t" << data_ptr->data << std::endl;
+	std::cout << "raw:\t\t" << raw << std::endl;
+	std::cout << "*new_ptr:\t" << new_ptr << std::endl;
+	std::cout << "new_ptr->data:\t" << new_ptr->data << std::endl;
+	delete data_ptr;
 	return 0;
 }
