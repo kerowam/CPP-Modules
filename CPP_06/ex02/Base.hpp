ff18800d2783f0ebdbc9b8459b966aafb8593d4d
gfredes- <gfredes-@student.42malaga.com>
2024-08-06 19:23:08 +0200

    ex02 of CPP_06 done. Pending to check

--- CPP_06/ex02/Base.hpp
+++ CPP_06/ex02/Base.hpp
@@ -6,18 +6,19 @@
 /*   By: gfredes- <gfredes-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 20:03:52 by gfredes-          #+#    #+#             */
-/*   Updated: 2024/08/05 20:06:33 by gfredes-         ###   ########.fr       */
+/*   Updated: 2024/08/06 19:18:56 by gfredes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BASE_HPP
 # define BASE_HPP
 # include <iostream>
+# include <exception>
 
 class Base
 {
 	public:
-		~Base(){};
+		virtual	~Base(){};
 };
 
 #endif
--- CPP_06/ex02/main.cpp
+++ CPP_06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gfredes- <gfredes-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/03 20:10:40 by gfredes-          #+#    #+#             */
-/*   Updated: 2024/08/05 20:11:04 by gfredes-         ###   ########.fr       */
+/*   Updated: 2024/08/06 19:21:44 by gfredes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,64 @@
 
 Base	*generate(void)
 {
+	Base	*base;
+	int		random_nbr;
 
+	srand(time(NULL));
+	random_nbr = rand() % 3;
+	switch(random_nbr)
+	{
+		case 0:
+			base = new A;
+			break;
+		case 1:
+			base = new B;
+			break;
+		case 2:
+			base = new C;
+	}
+	return base;
 }
 
 void	identify(Base* p)
 {
-
+	if (dynamic_cast<A*>(p))
+		std::cout << "The type of the object pointed is: A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "The type of the object pointed is: B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "The type of the object pointed is: C" << std::endl;
+	else
+		std::cout << "Error: type doesn't found!" << std::endl;
 }
 
 void	identify(Base& p)
 {
-
+	try
+	{
+		dynamic_cast<A&>(p);
+		std::cout << "The type of the object referenced is: A" << std::endl;
+	}
+	catch (std::exception& e) {}
+	try
+	{
+		dynamic_cast<B&>(p);
+		std::cout << "The type of the object referenced is: B" << std::endl;
+	}
+	catch (std::exception& e) {}
+	try
+	{
+		dynamic_cast<C&>(p);
+		std::cout << "The type of the object referenced is: C" << std::endl;
+	}
+	catch (std::exception& e) {}
 }
 
 int	main(void)
 {
-	
+	Base *base = generate();
+
+	identify(base);
+	identify(*base);
 	return 0;
 }
