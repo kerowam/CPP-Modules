d9d6786444b3f8763807237f674809ccfdb9d613
gfredes- <gfredes-@student.42malaga.com>
2024-08-10 23:56:33 +0200

    mendfiles and structure of ex02 in CPP_08 added

--- CPP_08/ex02/MutantStack.hpp
+++ CPP_08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: gfredes- <gfredes-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/10 23:41:57 by gfredes-          #+#    #+#             */
-/*   Updated: 2024/08/10 23:45:34 by gfredes-         ###   ########.fr       */
+/*   Updated: 2024/08/10 23:56:03 by gfredes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,16 @@
 # include <iostream>
 # include <stack>
 
-class MutantStack
-{
+template <typename T>
 
+class MutantStack: public std::stack<T>
+{
+	public:
+		MutantStack();
+		MutantStack(const MutantStack& mutantStack);
+		~MutantStack();
+
+		MutantStack&	operator=(const MutantStack& mutantStack);
 }
 
 #endif
