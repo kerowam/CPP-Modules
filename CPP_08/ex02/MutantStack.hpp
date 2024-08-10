a1e577a8c3ce739e8d166cbc8d6c77b21f2ba9bd
gfredes- <gfredes-@student.42malaga.com>
2024-08-10 23:55:50 +0200

    files and structure of ex02 in CPP_08 added

---
+++ CPP_08/ex02/MutantStack.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gfredes- <gfredes-@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/10 23:41:57 by gfredes-          #+#    #+#             */
+/*   Updated: 2024/08/10 23:45:34 by gfredes-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+# include <iostream>
+# include <stack>
+
+class MutantStack
+{
+
+}
+
+#endif
