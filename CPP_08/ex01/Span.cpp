6c6b43d61eeea526c253732d6b491f9018d78cd3
gfredes- <gfredes-@student.42malaga.com>
2024-08-10 19:19:39 +0200

    mendfiles and structure of ex01 in CPP_08 added

--- CPP_08/ex01/Span.cpp
+++ CPP_08/ex01/Span.cpp
@@ -6,8 +6,58 @@
 /*   By: gfredes- <gfredes-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/10 18:54:21 by gfredes-          #+#    #+#             */
-/*   Updated: 2024/08/10 18:55:13 by gfredes-         ###   ########.fr       */
+/*   Updated: 2024/08/10 19:19:15 by gfredes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
+
+Span::Span()
+{
+
+}
+
+Span::Span(unsigned int N)
+{
+
+}
+
+Span::Span(const Span& span)
+{
+
+}
+
+Span::~Span()
+{
+
+}
+
+Span&	Span::operator=(const Span& span)
+{
+
+}
+
+void	Span::addNumber(int nbr)
+{
+
+}
+
+int		Span::shortestSpan()
+{
+
+}
+
+int		Span::longestSpan()
+{
+
+}
+
+const char*	Span::FullArray::what()const throw()
+{
+
+}
+
+const char*	Span::InsufficientNumbers::what()const throw()
+{
+
+}
