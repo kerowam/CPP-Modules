a43b40436a93dded8b45cea8fea1aa5918e9bce7
gfredes- <gfredes-@student.42malaga.com>
2024-08-17 00:29:06 +0200

    canonical fts added

--- CPP_09/ex00/BitcoinExchange.cpp
+++ CPP_09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,66 @@
 /*   By: gfredes- <gfredes-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 14:55:43 by gfredes-          #+#    #+#             */
-/*   Updated: 2024/08/12 14:55:43 by gfredes-         ###   ########.fr       */
+/*   Updated: 2024/08/16 23:21:39 by gfredes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "BitcoinExchange.hpp"
+
+BitcoinExchange::BitcoinExchange() {}
+
+
+BitcoinExchange::BitcoinExchange(std::string file)
+{
+	std::ifstream 	data_file(file.c_str());
+	std::string		format;
+	std::string		line;
+	std::string		date;
+	float			price;
+
+	format = file.substr(file.length() - 4);
+	if (format != ".csv")
+	{
+		std::cerr << "Error: invalid format of data file." << std::endl;
+		exit(1);
+	}
+	if (!data_file.is_open())
+	{
+		std::cerr << "Error: could not open file." << std::endl;
+		exit(1);
+	}
+	std::getline(data_file, line);
+	while (std::getline(data_file, line))
+	{
+		std::stringstream	splitLine(line);
+		if (std::getline(splitLine, date, ',') && splitLine >> price)
+			_datesAndBtcPrices[date] = price;
+		else
+		{
+			std::cerr << "Error: invalid format of data file." << std::endl;
+			exit(1);
+		}
+	}
+}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange& bitcoinExchange)
+{
+	*this = bitcoinExchange;
+}
+
+BitcoinExchange::~BitcoinExchange()
+{
+
+}
+
+BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& bitcoinExchange)
+{
+	if (this != &bitcoinExchange)
+		_datesAndBtcPrices = bitcoinExchange._datesAndBtcPrices;
+	return *this;
+}
+		
+void	BitcoinExchange::getBtcprices(std::string fileName)
+{
+
+}
--- CPP_09/ex00/BitcoinExchange.hpp
+++ CPP_09/ex00/BitcoinExchange.hpp
@@ -6,13 +6,33 @@
 /*   By: gfredes- <gfredes-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 14:56:03 by gfredes-          #+#    #+#             */
-/*   Updated: 2024/08/12 14:56:40 by gfredes-         ###   ########.fr       */
+/*   Updated: 2024/08/16 23:29:44 by gfredes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BITCOINEXCHANGE_HPP
 # define BITCOINEXCHANGE_HPP
+# include <iostream>
+# include <map>
+# include <string>
+# include <fstream>
+# include <sstream>
 
+class BitcoinExchange
+{
+	private:
+		std::map<std::string, float> _datesAndBtcPrices;
 
+		BitcoinExchange();
+
+	public:
+		BitcoinExchange(std::string file);
+		BitcoinExchange(const BitcoinExchange& bitcoinExchange);
+		~BitcoinExchange();
+
+		BitcoinExchange& operator=(const BitcoinExchange& bitcoinExchange);
+		
+		void	getBtcprices(std::string fileName);
+};
 
 #endif
