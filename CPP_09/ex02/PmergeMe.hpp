0f12448f995b6b3b6aeac4d81971549509ea24c6
gfredes- <gfredes-@student.42malaga.com>
2024-08-19 01:31:17 +0200

    PmergeMe.hpp cleaned. Pending to check

--- CPP_09/ex02/PmergeMe.hpp
+++ CPP_09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: gfredes- <gfredes-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 20:12:28 by gfredes-          #+#    #+#             */
-/*   Updated: 2024/08/19 00:34:51 by gfredes-         ###   ########.fr       */
+/*   Updated: 2024/08/19 01:29:39 by gfredes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,6 @@ class PmergeMe
 		double				_listTime;
 
 		PmergeMe();
-		//void	vectorMergeInsertSort(std::vector<int>& v);
-		//void	vectorMerge(std::vector<int>& v, std::vector<int>& leftVector, std::vector<int>& rightVector);
 	public:
 		~PmergeMe();
 		PmergeMe(const PmergeMe& pmerge);
@@ -40,12 +38,9 @@ class PmergeMe
 
 		PmergeMe&	operator=(const PmergeMe& pmerge);
 		
-		//void	printVector(std::vector<int>& _vector);
 		void	printVector();
 		void	printList();
-		//void	sort(std::vector<int> &_vector);
 		void	sortVector();
-		//void	sort(std::list<int> &_list);
 		void	sortList();
 		void	putTimers();
 };
